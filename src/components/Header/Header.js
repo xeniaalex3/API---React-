74eb66970602c425074902fbf4dbe1612825c7f7
xeniaalex3 <xenia_onescu_3@hotmail.com>
2022-07-28 11:31:46 +0200

    install yarn and create components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import logo from 'src/assets/images/logo-github.png';
+
+function Header() {
+  return (
+    <div className="ui segment">
+      <img src={logo} alt="Logo Github" />
+    </div>
+  );
+}
+
+export default Header;
